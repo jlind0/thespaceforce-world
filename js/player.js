8ef5302e3c779a3f98d20f4143ad8b1e82b2f03b
Jason Lind <lind@yahoo.com>
2021-07-18 13:16:22 -0500

    lets try this

---
+++ js/player.js
@@ -0,0 +1,42 @@
+export function onYouTubeIframeAPIReady() {
+    StartVideo = new YouTubePlayer($("#topPlayer").get(0), "M6bumUQwQIU", 16.0 / 9.0);
+}
+var YouTubePlayer = (function () {
+    function YouTubePlayer(panel, videoId, aspectRatio) {
+        var _this = this;
+        this.panel = panel;
+        this.aspectRatio = aspectRatio;
+        var size = this.CalcVideoSize();
+        this.Player = new YT.Player(panel, {
+            videoId: videoId,
+            height: size.Height,
+            width: size.Width,
+            events: {
+                onReady: function () {
+                    _this.Player.playVideo();
+                }
+            },
+            playerVars: {
+                autoplay: YT.AutoPlay.AutoPlay,
+                playsinline: YT.PlaysInline.Inline
+            }
+        });
+        window.onresize = function () {
+            if (_this.Player != null) {
+                size = _this.CalcVideoSize();
+                _this.Player.setSize(size.Width, size.Height);
+            }
+        };
+    }
+    YouTubePlayer.prototype.CalcVideoSize = function () {
+        var width = Math.floor($(this.panel).width() * 0.98);
+        var height = Math.floor(width * this.aspectRatio);
+        return {
+            Width: width,
+            Height: height
+        };
+    };
+    return YouTubePlayer;
+}());
+export { YouTubePlayer };
+//# sourceMappingURL=player.js.map
---
+++ js/player.ts
@@ -0,0 +1,46 @@
+///<reference path="../node_modules/@types/jquery/index.d.ts"/>
+///<reference path="../node_modules/@types/knockout/index.d.ts"/>
+///<reference path="../node_modules/@types/youtube/index.d.ts"/>
+///<reference path="../node_modules/@types/youtube-player/index.d.ts"/>
+declare var StartVideo : YouTubePlayer;
+export function onYouTubeIframeAPIReady() : void{
+    StartVideo = new YouTubePlayer($("#topPlayer").get(0), "M6bumUQwQIU", 16.0/9.0);
+}
+export class YouTubePlayer{
+    protected Player : YT.Player;
+    constructor(protected panel : HTMLElement, videoId: string, protected aspectRatio : number){
+        var size = this.CalcVideoSize();
+        this.Player = new YT.Player(panel, {
+            videoId: videoId,
+            height: size.Height,
+            width: size.Width,
+            events: {
+                onReady: () =>{
+                    this.Player.playVideo();
+                }
+            },
+            playerVars:{
+                autoplay: YT.AutoPlay.AutoPlay,
+                playsinline: YT.PlaysInline.Inline
+            }
+        });
+        window.onresize = ()=>{
+            if(this.Player != null){
+                size = this.CalcVideoSize();
+                this.Player.setSize(size.Width, size.Height);
+            }
+        };
+    }
+    protected CalcVideoSize() : VideoSize{
+        var width = Math.floor($(this.panel).width() * 0.98);
+        var height = Math.floor(width*this.aspectRatio)
+        return{
+            Width: width,
+            Height: height
+        }
+    }
+}
+export interface VideoSize{
+    Width: number;
+    Height: number;
+}
