94632608946d7ce5e3d5d392b2ac3855f041f85d
jlind0 <lind@yahoo.com>
2021-08-30 23:43:31 -0500

    major updates

--- js/ApplyViewModel.ts
+++ js/ApplyViewModel.ts
@@ -30,6 +30,16 @@ export interface Position{
     requiredFields?: CareerFields[]
 
 }
+export interface CareerFieldData{
+    careerFields: CareerField[]
+}
+export interface CareerField{
+    name: string,
+    id: CareerFields,
+    color: string,
+    forecolor: string,
+    description: string
+}
 export const enum RankType{
     nato = "nato",
     un = "un"
@@ -52,26 +62,30 @@ export const enum CareerFields{
 }
 type FetchParams = Parameters<typeof window.fetch>;
 const fetchJson = <T>(...params: FetchParams) : Promise<T> => window.fetch(...params).then((resp) => resp.json() as Promise<T>);
+
 document.addEventListener('DOMContentLoaded', () =>{
-   var cats = fetchJson<Categories>("files/positions.json").then(data =>{
-    var vm = new ApplyViewModel(data.categories);
-    ko.applyBindings(vm);
-    vm.addClickEvent();
-    $('.carousel').carousel();
-    $(".carousel").each((i,e) =>{
-        e.addEventListener("slide.bs.carousel", (evt) =>{
-            var ctx = <CategoryViewModel> ko.dataFor(e);
-            var positions = ctx.Positions();
-            for(let j = 0; j < positions.length; j++){
-                positions[j].Selected(j === evt.to);
-            }
+  fetchJson<Categories>("files/positions.json").then(data =>{
+    var fields = fetchJson<CareerFieldData>("files/careerFields.json").then((cfData) =>{
+        var vm = new ApplyViewModel(data.categories, cfData);
+        ko.applyBindings(vm);
+        vm.addClickEvent();
+        $('.carousel').carousel();
+        $(".carousel").each((i,e) =>{
+            e.addEventListener("slide.bs.carousel", (evt) =>{
+                var ctx = <CategoryViewModel> ko.dataFor(e);
+                var positions = ctx.Positions();
+                for(let j = 0; j < positions.length; j++){
+                    positions[j].Selected(j === evt.to);
+                }
+            });
         });
-    })
+    });
+   
    });
 });
 export class ApplyViewModel{
     public Categories : KnockoutObservableArray<CategoryViewModel>;
-    constructor(data: Category[]){
+    constructor(data: Category[], public careerFields: CareerFieldData){
         this.Categories = ko.observableArray();
          for(let i = 0; i< data.length; i++){
             this.Categories.push(new CategoryViewModel(this, data[i], i == 0));
@@ -88,6 +102,22 @@ export class ApplyViewModel{
         for(let i = 0; i < cats.length; i++){
             cats[i].Selected(cats[i].TabId() === tabId);
         }
+    }
+}
+export class CareerFieldViewModel {
+    public Name: KnockoutObservable<string>;
+    public Id: KnockoutObservable<string>;
+    public Color: KnockoutObservable<string>;
+    public ForeColor: KnockoutObservable<string>;
+    public Description: KnockoutObservable<string>;
+    public IsRequired: KnockoutObservable<boolean>;
+    constructor(careerField: CareerField, isRequired = false) {
+        this.Name = ko.observable(careerField.name);
+        this.Id = ko.observable(careerField.id);
+        this.Color = ko.observable(careerField.color);
+        this.ForeColor = ko.observable(careerField.forecolor);
+        this.Description = ko.observable(careerField.description);
+        this.IsRequired = ko.observable(isRequired);
     }
 }
 export class CategoryViewModel{
@@ -135,7 +165,7 @@ export class CategoryViewModel{
         this.Positions = ko.observableArray();
         if(data.positions !== undefined){
             for(let i = 0; i < data.positions.length; i++){
-                this.Positions.push(new PositionViewModel(data.positions[i], i));
+                this.Positions.push(new PositionViewModel(this, data.positions[i], i));
             }
         }
     }
@@ -153,7 +183,7 @@ export class CategoryViewModel{
         public Index: KnockoutObservable<number>;
         public ApplyActive: KnockoutComputed<string>;
         public Selected: KnockoutObservable<boolean>;
-        constructor(protected position: Position, protected index: number) {
+        constructor(protected parent: CategoryViewModel,protected position: Position, protected index: number) {
             this.Selected = ko.observable(this.index === 0);
             this.ApplyActive = ko.computed(() =>{
                 return this.Selected() ? "active" : "";
